7357d2292f449f930cc5e9fd5f3838f74b523c8c
Wesley Costa <wesley.costa.developer@gmail.com>
2020-01-11 18:36:33 -0300

    Projeto criado

---
+++ src/main/java/com/pokepoint/PokepointApplication.java
@@ -0,0 +1,13 @@
+package com.pokepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokepointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokepointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokepoint/PokepointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokepoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokepointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
