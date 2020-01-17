d2b9361161d628668e0be907ebebb7b68df5c92f
Wesley Costa <wesley.costa.developer@gmail.com>
2020-01-17 13:52:40 -0300

    removendo a versão em espanhol do nome do movimento e do tipo

--- src/main/java/com/pokepoint/domain/MoveName.java
+++ src/main/java/com/pokepoint/domain/MoveName.java
@@ -1,6 +1,7 @@
 package com.pokepoint.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class MoveName implements Serializable{
 	/**
@@ -12,14 +13,12 @@ public class MoveName implements Serializable{
 	private String englishName;
 	private String japaneseName;
 	private String portugueseName;
-	private String spanishName;
-	
-	public MoveName(Integer id, String englishName, String japaneseName, String portugueseName, String spanishName) {
+
+	public MoveName(Integer id, String englishName, String japaneseName, String portugueseName) {
 		this.id = id;
 		this.englishName = englishName;
 		this.japaneseName = japaneseName;
 		this.portugueseName = portugueseName;
-		this.spanishName = spanishName;
 	}
 
 	public Integer getId() {
@@ -38,10 +37,6 @@ public class MoveName implements Serializable{
 		return this.portugueseName;
 	}
 
-	public String getSpanishName() {
-		return this.spanishName;
-	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
@@ -58,57 +53,20 @@ public class MoveName implements Serializable{
 		this.portugueseName = portugueseName;
 	}
 
-	public void setSpanishName(String spanishName) {
-		this.spanishName = spanishName;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		MoveName moveName = (MoveName) o;
+		return Objects.equals(id, moveName.id) &&
+				Objects.equals(englishName, moveName.englishName) &&
+				Objects.equals(japaneseName, moveName.japaneseName) &&
+				Objects.equals(portugueseName, moveName.portugueseName);
 	}
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((englishName == null) ? 0 : englishName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((japaneseName == null) ? 0 : japaneseName.hashCode());
-		result = prime * result + ((portugueseName == null) ? 0 : portugueseName.hashCode());
-		result = prime * result + ((spanishName == null) ? 0 : spanishName.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MoveName other = (MoveName) obj;
-		if (englishName == null) {
-			if (other.englishName != null)
-				return false;
-		} else if (!englishName.equals(other.englishName))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (japaneseName == null) {
-			if (other.japaneseName != null)
-				return false;
-		} else if (!japaneseName.equals(other.japaneseName))
-			return false;
-		if (portugueseName == null) {
-			if (other.portugueseName != null)
-				return false;
-		} else if (!portugueseName.equals(other.portugueseName))
-			return false;
-		if (spanishName == null) {
-			if (other.spanishName != null)
-				return false;
-		} else if (!spanishName.equals(other.spanishName))
-			return false;
-		return true;
+		return Objects.hash(id, englishName, japaneseName, portugueseName);
 	}
 
 	@Override
@@ -123,7 +81,6 @@ public class MoveName implements Serializable{
 		builder.append(", portugueseName=");
 		builder.append(portugueseName);
 		builder.append(", spanishName=");
-		builder.append(spanishName);
 		builder.append("]");
 		return builder.toString();
 	}
--- src/test/java/com/pokepoint/domain/TesteMoveName.java
+++ src/test/java/com/pokepoint/domain/TesteMoveName.java
@@ -9,16 +9,14 @@ public class TesteMoveName {
 	private static final String ENGLISH_NAME = "Dragon Claw";
 	private static final String JAPANESE_NAME = "ドラゴンクロー";
 	private static final String PORTUGUESE_NAME = "Garra de Dragão";
-	private static final String SPANISH_NAME = "Garra de Dragón";
-	
+
 	@Test
 	public void criarObjeto() {
-		MoveName moveName = new MoveName(ID, ENGLISH_NAME, JAPANESE_NAME, PORTUGUESE_NAME, SPANISH_NAME);
+		MoveName moveName = new MoveName(ID, ENGLISH_NAME, JAPANESE_NAME, PORTUGUESE_NAME);
 		assertEquals(moveName.getId(), ID);
 		assertEquals(moveName.getEnglishName(), ENGLISH_NAME);
 		assertEquals(moveName.getJapaneseName(), JAPANESE_NAME);
 		assertEquals(moveName.getPortugueseName(), PORTUGUESE_NAME);
-		assertEquals(moveName.getSpanishName(), SPANISH_NAME);
 		System.out.println(moveName);
 	}
 }
