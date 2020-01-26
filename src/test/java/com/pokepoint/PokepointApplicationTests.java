b9e95b7aca02ae75386dd3cd736942902e1e95cf
Wesley Costa <wesley.costa.developer@gmail.com>
2020-01-25 21:30:01 -0300

    Implementado test para as requisições de tipo de pokemon

--- src/test/java/com/pokepoint/PokepointApplicationTests.java
+++ src/test/java/com/pokepoint/PokepointApplicationTests.java
@@ -1,13 +1,18 @@
 package com.pokepoint;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class PokepointApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PokepointApplication.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class PokepointApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
