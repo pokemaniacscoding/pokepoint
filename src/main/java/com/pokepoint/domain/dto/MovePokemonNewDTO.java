47fe4c08bf7c997830503d31b13a3055b2ee0aca
Wesley Costa <wesley.costa.developer@gmail.com>
2020-01-26 21:36:06 -0300

    Implementado o insert de movimento de pokemon

---
+++ src/main/java/com/pokepoint/domain/dto/MovePokemonNewDTO.java
@@ -0,0 +1,93 @@
+package com.pokepoint.domain.dto;
+
+import com.pokepoint.validation.MoveInsert;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@MoveInsert
+public class MovePokemonNewDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Valid
+    @NotEmpty(message = "Preenchimento obrigatório!!!")
+    private String englishName;
+    @NotEmpty(message = "Preenchimento obrigatório!!!")
+    private String japaneseName;
+    @NotEmpty(message = "Preenchimento obrigatório!!!")
+    private String portugueseName;
+    private Integer accuracy;
+    private Integer power;
+    private Integer ppMin;
+    private Integer ppMax;
+    private Integer typeId;
+
+    public MovePokemonNewDTO() {
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    public void setEnglishName(String englishName) {
+        this.englishName = englishName;
+    }
+
+    public String getJapaneseName() {
+        return japaneseName;
+    }
+
+    public void setJapaneseName(String japaneseName) {
+        this.japaneseName = japaneseName;
+    }
+
+    public String getPortugueseName() {
+        return portugueseName;
+    }
+
+    public void setPortugueseName(String portugueseName) {
+        this.portugueseName = portugueseName;
+    }
+
+    public Integer getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(Integer accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
+    }
+
+    public Integer getPpMin() {
+        return ppMin;
+    }
+
+    public void setPpMin(Integer ppMin) {
+        this.ppMin = ppMin;
+    }
+
+    public Integer getPpMax() {
+        return ppMax;
+    }
+
+    public void setPpMax(Integer ppMax) {
+        this.ppMax = ppMax;
+    }
+
+}
